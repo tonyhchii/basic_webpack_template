60ad728ddcb516e2b6656e24abf7acccbf7930e0
Tony Huang <170560060+tonyhchii@users.noreply.github.com>
2024-09-11 17:15:46 -0400

    yea

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,21 +6,5 @@ module.exports = merge(common, {
     devtool: "eval-source-map",
     devServer: {
         watchFiles: ["./src/template.html"],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.html$/i,
-                loader: "html-loader",
-              },
-              {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
-              },
-        ],
-    },
+    }, 
 })
